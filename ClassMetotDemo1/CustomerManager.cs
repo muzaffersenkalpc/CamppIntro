1184fce89a4d4bfad9570417876954b9ec00ed8c
Muzaffer Şenkal <39282226+muzaffersenkalpc@users.noreply.github.com>
2023-06-07 17:07:40 +0300

    Add files via upload

---
+++ ClassMetotDemo1/CustomerManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo1
+{
+    internal class CustomerManager
+    {
+       public void Add(Customer customer)
+        {
+            Console.WriteLine("Basariyla eklendi : " + customer.Name);
+        }
+        public void Update(Customer customer) 
+        {
+            Console.WriteLine("Basariyla gncellendi : " + customer.Name);
+        }
+    }
+}
---
+++ ClassMetotDemo1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using ClassMetotDemo1;
+
+Customer customer = new Customer();
+customer.Name = "Muzo";
+customer.Surname = "senkal";
+customer.Id = 1;
+Customer customer2 = new Customer();
+customer2.Name = "Ahmet";
+customer2.Surname = "gg";
+customer2.Id = 2;
+
+Customer[] customers = new Customer[] { customer, customer2 };
+
+foreach (Customer x in customers)
+{
+    Console.WriteLine(x.Name);
+
+}
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(customer2);
